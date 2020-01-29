8232a3d265e2d743ee0488e778c449b1e654ca6b
Shiva Gunasehkaran <danielshiva@gmail.com>
2020-01-29 14:29:22 +0530

    Integrated Redux and API service call

--- src/views/screens/Home/Home.js
+++ src/views/screens/Home/Home.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import store from './Reducers/Index';
+import ServiceCallAction from './Actions/ServiceCallAction';
 import theme from '../../../themes';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 class Home extends Component {
     static navigationOptions = ({navigation}) => ({
-        title: ('Planets list'),
+        title: ('Breweries list'),
         headerStyle: {
             backgroundColor: theme.colors.buttonColor,
         },
+        headerRight: () => <Icon style={{paddingRight: 15, color: theme.colors.black}}
+                                 name="plus"
+                                 size={20}/>,
     });
 
     render() {
         return (
-            <View></View>
+            <Provider store={store}>
+                <ServiceCallAction/>
+            </Provider>
         );
     }
 }
