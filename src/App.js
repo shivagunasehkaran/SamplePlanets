98ac4aafa975d98f21a77c6fff9d614db998f038
Shiva Gunasehkaran <danielshiva@gmail.com>
2020-01-29 13:10:52 +0530

    Initial folder setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>{('Test')}</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
